7f6d222b93ad6eb5700dd921d8aa43bdf4c6eca1
jake <jake.feiglin@gmail.com>
2020-04-22 16:01:32 +0300

    actually working

--- src/main.c
+++ src/main.c
@@ -12,7 +12,6 @@
 
 int g_major;		/* g_major number assigned to our device driver */
 asmlinkage int (*original_execve)(const char *, char * const*, char * const*);
-
 unsigned long *sys_call_table;
 
 const struct file_operations random_fops = {
@@ -27,12 +26,20 @@ const struct file_operations random_fops = {
 
 asmlinkage int disrupted_execve(const char *filename, char *const argv[], char *const envp[], char *k_filename){
 	int retval = 0;
+	mm_segment_t old_fs;
+
 	printk(KERN_INFO "PID %d (%s) called execve to execute %s. Adding LD_PRELOAD\n", current->pid, current->comm, k_filename);
-	//display_envp_values(envp);
+
+	//by setting the fs register to KERNEL_DS, we disable the kernel memory access checks and make 
+	// the execve syscall accept the fact that one of the pointers in envp is a pointer to kernel space
+	old_fs = get_fs();
+	set_fs(KERNEL_DS);
+	
 	insert_envp_value(LD_PRELOAD_ENV_VAR, envp);
 	retval = original_execve(filename, argv, envp);
-	display_envp_values(envp);
-	//printk(KERN_INFO "PID %d (%s) returned from execve to execute %s w/ retval %d\n", current->pid, current->comm, k_filename, retval);
+	
+	set_fs(old_fs);
+	
 	return retval;
 }
 
@@ -51,7 +58,7 @@ int init_module(void)
 {
 	sys_call_table = (unsigned long *)kallsyms_lookup_name("sys_call_table");
 	printk(KERN_INFO "System call table located at: %lx\n", (long unsigned int)sys_call_table);
-	
+
 	original_execve = (void*)sys_call_table[__NR_execve];
 	printk(KERN_INFO "Hooking execve at offset %d, address %lx\n", __NR_execve, (long unsigned int)original_execve);
 	hooking_syscall(sys_call_table, hooked_execve, __NR_execve);
